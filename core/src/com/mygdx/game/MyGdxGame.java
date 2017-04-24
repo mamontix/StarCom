9a17f0fc8f94dfae68150709cbc01f0c4fe5047b
mamontix <daemonix@bk.ru>
2017-04-24 18:05:12 +1000

    StarCom 1.0

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -9,23 +9,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
+
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
-	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(img,0,300);
 		batch.end();
 	}
-	
-	@Override
+
+	public void update() {
+
+	}
+
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
